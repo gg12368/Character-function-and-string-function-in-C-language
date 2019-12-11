95b8618af5baecfb02a1fbcd9767b91f7e14ffad
gg12368 <56186550+gg12368@users.noreply.github.com>
2019-12-11 21:54:04 +0800

    Update and rename strstr.c to strstr2.c

--- strstr2.c
+++ strstr2.c
@@ -1,12 +1,12 @@
 strstr() 函数
 作用：搜索一个字符串在另一个字符串中的第一次出现。该函数返回字符串的其余部分（从匹配点）。如果未找到所搜索的字符串，则返回 false。该函数是二进制安全的。
-例1：
-
+例2：
 int main()
 { 
 	char str[] = "This is a simple string";
 	char* pch= strstr(str, "simple");
-	puts(pch);
+	strncpy(pch, "sample", 6);
+	puts(str);
 	system("pause");
 	return 0;
 }
